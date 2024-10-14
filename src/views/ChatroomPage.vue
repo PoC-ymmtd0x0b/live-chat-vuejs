67108b57b45a24eef6ade9c7bff720c8a5ab2c7e
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-10-14 16:30:29 +0900

    チャットの入力機能の実装

--- src/views/ChatroomPage.vue
+++ src/views/ChatroomPage.vue
@@ -2,16 +2,19 @@
   <div class="container">
     <NavBar />
     <ChatWindow :messages="messages" />
+    <NewChatForm @connectCable="connectCable" />
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue'
 import ChatWindow from '@/components/ChatWindow.vue'
+import NewChatForm from '@/components/NewChatForm.vue'
 import axios from 'axios'
+import ActionCable from 'actioncable'
 
 export default {
-  components: { NavBar, ChatWindow },
+  components: { NavBar, ChatWindow, NewChatForm },
   data() {
     return {
       messages: [],
@@ -37,9 +40,26 @@ export default {
         console.log(err)
       }
     },
+    connectCable(message) {
+      this.messageChannel.perform('receive', {
+        message: message,
+        email: window.localStorage.getItem('uid'),
+      })
+    },
   },
   mounted() {
-    this.getMessages()
+    const cable = ActionCable.createConsumer('ws://localhost:3000/cable')
+    this.messageChannel = cable.subscriptions.create('RoomChannel', {
+      connected: () => {
+        this.getMessages()
+      },
+      received: () => {
+        this.getMessages()
+      },
+    })
+  },
+  beforeUnmount() {
+    this.messageChannel.unsubscribe()
   },
 }
 </script>
